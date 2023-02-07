1b14d82ec98a082894fde4f07d6dd6fb0a38805b
celestewv <cwalstromvangor@ucsd.edu>
2023-02-06 20:47:05 -0800

    changes

--- FileServer.java
+++ FileServer.java
@@ -13,24 +13,25 @@ class FileHelpers {
     static List<File> getFiles(Path start) throws IOException {
         File f = start.toFile();
         List<File> result = new ArrayList<>();
-        if(f.isDirectory()) {
+        if (f.isDirectory()) {
             File[] paths = f.listFiles();
-            for(File subFile: paths) {
+            for (File subFile : paths) {
                 result.addAll(getFiles(subFile.toPath()));
             }
-        }
-        else {
+        } else {
             result.add(start.toFile());
         }
         return result;
     }
+
     static String readFile(File f) throws IOException {
         return new String(Files.readAllBytes(f.toPath()));
     }
+
     static String repeat(String s, int n) {
         String result = "";
-        for(int i = 0; i < n; i += 1) {
-          result += s;
+        for (int i = 0; i < n; i += 1) {
+            result += s;
         }
         return result;
     }
@@ -38,68 +39,67 @@ class FileHelpers {
 
 class Handler implements URLHandler {
     List<File> paths;
+
     Handler(String directory) throws IOException {
-      this.paths = FileHelpers.getFiles(Paths.get(directory));
+        this.paths = FileHelpers.getFiles(Paths.get(directory));
     }
+
     public String handleRequest(URI url) throws IOException {
-       if (url.getPath().equals("/")) {
-           return String.format("Use /search?q=... to search the files!", paths.size());
-       } else if (url.getPath().equals("/search")) {
-           String[] parameters = url.getQuery().split("=");
-           if (parameters[0].equals("q")) {
-               List<File> foundPaths = new ArrayList<>();
-               for(File f: paths) {
-                   if(f.toString().contains(parameters[1])) {
-                       foundPaths.add(f);
-                   }
-               }
-               Collections.sort(foundPaths);
-               String result = "";
-               String border = FileHelpers.repeat("=", 72);
-               for (File f: foundPaths) {
-                  String contents = FileHelpers.readFile(f);
-                  contents = contents.length() > 100 ? contents.substring(0, 100) : contents;
-                  result += border + "\n" + f.toString() + ":\n" + contents + "...\n" + border + "\n";
-               }
-               if(foundPaths.size() == 0) {
-                 return "No files found";
-               }
-               else {
-                 return String.format("Found %d files:\n%s", foundPaths.size(), result);
-               }
-           }
-           else {
-               return "Couldn't find query parameter q";
-           }
-       }
-       else {
-           return "Don't know how to handle that path!";
-       }
+        if (url.getPath().equals("/")) {
+            return String.format("Use /search?q=... to search the files!", paths.size());
+        } else if (url.getPath().equals("/search")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("q")) {
+                List<File> foundPaths = new ArrayList<>();
+                for (File f : paths) {
+                    if (f.toString().contains(parameters[1])) {
+                        foundPaths.add(f);
+                    }
+                }
+                Collections.sort(foundPaths);
+                String result = "";
+                String border = FileHelpers.repeat("=", 72);
+                for (File f : foundPaths) {
+                    String contents = FileHelpers.readFile(f);
+                    contents = contents.length() > 100 ? contents.substring(0, 100) : contents;
+                    result += border + "\n" + f.toString() + ":\n" + contents + "...\n" + border + "\n";
+                }
+                if (foundPaths.size() == 0) {
+                    return "No files found";
+                } else {
+                    return String.format("Found %d files:\n%s", foundPaths.size(), result);
+                }
+            } else {
+                return "Couldn't find query parameter q";
+            }
+        } else {
+            return "Don't know how to handle that path!";
+        }
     }
 }
 
 /**
-
-  Usage:
-
-  java FileServer <port> <directory>
-
-  Starts the server on <port>, with a Handler configured to search the given
-  directory when processing /search requests.
-
-  The server is designed to process requests like /search?q=<string> and
-  respond with the paths and contents of all files whose path matches the given
-  string. So `q=ch` as the query would return files with names like "ch1.txt"
-  or "march.txt".
-
-*/
+ * 
+ * Usage:
+ * 
+ * java FileServer <port> <directory>
+ * 
+ * Starts the server on <port>, with a Handler configured to search the given
+ * directory when processing /search requests.
+ * 
+ * The server is designed to process requests like /search?q=<string> and
+ * respond with the paths and contents of all files whose path matches the given
+ * string. So `q=ch` as the query would return files with names like "ch1.txt"
+ * or "march.txt".
+ * 
+ */
 class FileServer {
     public static void main(String[] args) throws IOException {
-        if(args.length < 1){
+        if (args.length < 1) {
             System.out.println("Missing port number! Try any number between 1024 to 49151 as the first argument");
             return;
         }
-        if(args.length < 2){
+        if (args.length < 2) {
             System.out.println("Missing directory to search! Give the path to a directory of text files to search.");
             return;
         }
@@ -109,4 +109,3 @@ class FileServer {
         Server.start(port, new Handler(args[1]));
     }
 }
-
